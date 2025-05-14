993dc5ac9805b8f2e24142baa7d73d633a2e2bcb
Mateian <bejmatei90@gmail.com>
2025-05-14 23:28:15 +0300

    database manager structure

---
+++ Clinica Medicala/ClinicaMedicalaForm/ClinicaMedicalaForm/components/Model/DatabaseManager/DatabaseManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicaMedicalaForm.components.Model.DatabaseManager
+{
+    public class DatabaseManager
+    {
+        private string _connectionString;
+
+        public DatabaseManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private SQLiteConnection GetConnection()
+        {
+            return new SQLiteConnection(_connectionString);
+        }
+
+        public SQLiteDataReader ExecuteSelectQuery(string query)
+        {
+            var connection = GetConnection();
+            connection.Open();
+            var command = new SQLiteCommand(query, connection);
+            return command.ExecuteReader();
+        }
+    }
+}
